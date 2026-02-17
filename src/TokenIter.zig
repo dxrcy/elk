eabd449db7146d73ae74bb0a40ca1019b345bc81
darcy <acednes@gmail.com>
2026-02-17 17:01:52 +1100

    refactor(parse): rewrite most `TokenIter` methods

--- src/TokenIter.zig
+++ src/TokenIter.zig
@@ -1,6 +1,7 @@
 const TokenIter = @This();
 
 const std = @import("std");
+const assert = std.debug.assert;
 
 const Operand = @import("Air.zig").Operand;
 const Lexer = @import("Lexer.zig");
@@ -10,87 +11,105 @@ const Integer = @import("integers.zig").Integer;
 const Reporter = @import("Reporter.zig");
 
 lexer: Lexer,
-token_peeked: ?Token,
+// Peek+peek or peek+next will parse same span as token multiple times, but this
+// is okay as it avoids storing an error union.
+peeked: ?Span,
 
 source: []const u8,
 reporter: *Reporter,
+
+const TokenTag = std.meta.Tag(Token.Value);
 
 pub fn new(source: []const u8, reporter: *Reporter) TokenIter {
     return .{
         .source = source,
         .reporter = reporter,
         .lexer = Lexer.new(source),
-        .token_peeked = null,
+        .peeked = null,
     };
 }
 
-pub fn discardRestOfLine(tokens: *TokenIter) void {
-    while (true) {
-        // TODO: Why using `nextToken` here ?
-        const token = tokens.nextToken(&.{}) catch |err| switch (err) {
-            // Ignore any other errors on this line
-            error.Reported => continue,
-        };
-        if (token == null or token.?.value == .newline)
-            break;
-    }
+pub fn getIndex(tokens: *const TokenIter) usize {
+    // FIXME: This doesn't work when a token is peeked
+    return tokens.lexer.index;
 }
 
-pub fn nextToken(
-    tokens: *TokenIter,
-    comptime skip: []const std.meta.Tag(Token.Value),
-) error{Reported}!?Token {
-    token: while (true) {
-        const token = try tokens.nextTokenAny() orelse
-            return null;
-        for (skip) |skip_kind| {
-            if (token.value == skip_kind)
-                continue :token;
-        }
-        return token;
-    }
+fn getNextSpan(tokens: *TokenIter) error{Eof}!Span {
+    return tokens.peeked orelse
+        tokens.lexer.next() orelse
+        return error.Eof;
 }
 
-pub fn discardOptionalToken(tokens: *TokenIter, comptime kind: std.meta.Tag(Token.Value)) !void {
-    if (try tokens.peekTokenAny()) |peeked| {
-        if (peeked.value == kind) {
-            _ = tokens.nextTokenAny() catch
-                unreachable orelse
-                unreachable;
-        }
-    }
-}
+fn next(tokens: *TokenIter) error{ Reported, Eof }!Token {
+    const span = try tokens.getNextSpan();
+    tokens.peeked = null;
 
-fn peekTokenAny(tokens: *TokenIter) !?Token {
-    if (tokens.token_peeked) |peeked| {
-        return peeked;
-    }
-    tokens.token_peeked = try tokens.nextTokenAny();
-    return tokens.token_peeked;
-}
-
-fn nextTokenAny(tokens: *TokenIter) !?Token {
-    if (tokens.token_peeked) |peeked| {
-        tokens.token_peeked = null;
-        return peeked;
-    }
-    const span = tokens.lexer.next() orelse
-        return null;
     return Token.from(span, tokens.source) catch |err| {
         try tokens.reporter.err(err, span);
     };
 }
 
-fn expectToken(tokens: *TokenIter) !Token {
-    const token = try tokens.nextToken(&.{}) orelse {
-        try tokens.reporter.err(error.UnexpectedEof, .emptyAt(tokens.source.len));
-    };
-    switch (token.value) {
-        .newline => {
-            try tokens.reporter.err(error.UnexpectedEol, .emptyAt(token.span.offset));
-        },
-        else => return token,
+/// Does **not** report failure to parse token.
+fn peek(tokens: *TokenIter) error{ InvalidTokenPeeked, Eof }!Token {
+    const span = try tokens.getNextSpan();
+    tokens.peeked = span;
+
+    return Token.from(span, tokens.source) catch
+        return error.InvalidTokenPeeked;
+}
+
+pub fn nextExcluding(
+    tokens: *TokenIter,
+    comptime discards: []const TokenTag,
+) error{ Reported, Eof }!Token {
+    token: while (true) {
+        const token = try tokens.next();
+        for (discards) |discard| {
+            if (token.value == discard)
+                continue :token;
+        }
+        return token;
     }
+    comptime unreachable;
+}
+
+pub fn discardOptional(
+    tokens: *TokenIter,
+    // This can become a slice if necessary
+    comptime discard: TokenTag,
+) void {
+    const token = tokens.peek() catch |err| switch (err) {
+        // These can be handled by next token request
+        error.InvalidTokenPeeked, error.Eof => return,
+    };
+    if (token.value == discard) {
+        assert(tokens.peeked != null);
+        tokens.peeked = null;
+    }
+}
+
+pub fn discardRemainingLine(tokens: *TokenIter) void {
+    while (true) {
+        const token = tokens.next() catch |err| switch (err) {
+            error.Reported => continue,
+            // This can be handled by next token request
+            error.Eof => break,
+        };
+        if (token.value == .newline)
+            break;
+    }
+}
+
+pub fn expectArgument(
+    tokens: *TokenIter,
+    comptime argument: Argument,
+) error{ Reported, Eof }!Operand.Spanned(argument.asType()) {
+    // TODO: Rewrite this too...
+    const token = try tokens.next();
+    const value = argument.convertValue(token.value) catch |err| {
+        try tokens.reporter.err(err, token.span);
+    };
+    return .{ .span = token.span, .value = value };
 }
 
 pub const Argument = union(enum) {
@@ -98,68 +117,58 @@ pub const Argument = union(enum) {
     word,
     string,
 
-    pub fn asType(comptime argument: Argument) type {
+    fn asType(comptime argument: Argument) type {
         return switch (argument) {
             .operand => |operand| operand,
             .word => Integer(16),
             .string => Span,
         };
     }
+
+    // TODO: Rename
+    fn convertValue(
+        comptime argument: Argument,
+        value: Token.Value,
+    ) error{ UnexpectedTokenKind, IntegerTooLarge }!argument.asType() {
+        return switch (argument) {
+            .word => return switch (value) {
+                .integer => |integer| integer,
+                else => error.UnexpectedTokenKind,
+            },
+            .string => return switch (value) {
+                .string => |string| string,
+                else => error.UnexpectedTokenKind,
+            },
+            .operand => |operand| switch (operand) {
+                Operand.Value.Register => switch (value) {
+                    .register => |register| .{ .inner = register },
+                    else => error.UnexpectedTokenKind,
+                },
+                Operand.Value.RegImm5 => switch (value) {
+                    .register => |register| .{ .register = register },
+                    .integer => |integer| .{ .immediate = try integer.castTo(u5) },
+                    else => error.UnexpectedTokenKind,
+                },
+                Operand.Value.Offset6 => switch (value) {
+                    .integer => |integer| .{ .inner = try integer.castTo(i6) },
+                    else => error.UnexpectedTokenKind,
+                },
+                Operand.Value.PCOffset9 => switch (value) {
+                    .integer => |integer| .{ .resolved = try integer.castTo(i9) },
+                    .label => .unresolved,
+                    else => error.UnexpectedTokenKind,
+                },
+                Operand.Value.PCOffset11 => switch (value) {
+                    .integer => |integer| .{ .resolved = try integer.castTo(i11) },
+                    .label => .unresolved,
+                    else => error.UnexpectedTokenKind,
+                },
+                Operand.Value.TrapVect => switch (value) {
+                    .integer => |integer| .{ .inner = try integer.castTo(u8) },
+                    else => error.UnexpectedTokenKind,
+                },
+                else => comptime unreachable,
+            },
+        };
+    }
 };
-
-pub fn expectArgument(
-    tokens: *TokenIter,
-    comptime argument: Argument,
-) !Operand.Spanned(argument.asType()) {
-    const token = try tokens.expectToken();
-    const value = convertArgument(argument, token.value) catch |err| {
-        try tokens.reporter.err(err, token.span);
-    };
-    return .{ .span = token.span, .value = value };
-}
-
-fn convertArgument(
-    comptime argument: Argument,
-    value: Token.Value,
-) error{ UnexpectedTokenKind, IntegerTooLarge }!argument.asType() {
-    return switch (argument) {
-        .word => return switch (value) {
-            .integer => |integer| integer,
-            else => error.UnexpectedTokenKind,
-        },
-        .string => return switch (value) {
-            .string => |string| string,
-            else => error.UnexpectedTokenKind,
-        },
-        .operand => |operand| switch (operand) {
-            Operand.Value.Register => switch (value) {
-                .register => |register| .{ .inner = register },
-                else => error.UnexpectedTokenKind,
-            },
-            Operand.Value.RegImm5 => switch (value) {
-                .register => |register| .{ .register = register },
-                .integer => |integer| .{ .immediate = try integer.castTo(u5) },
-                else => error.UnexpectedTokenKind,
-            },
-            Operand.Value.Offset6 => switch (value) {
-                .integer => |integer| .{ .inner = try integer.castTo(i6) },
-                else => error.UnexpectedTokenKind,
-            },
-            Operand.Value.PCOffset9 => switch (value) {
-                .integer => |integer| .{ .resolved = try integer.castTo(i9) },
-                .label => .unresolved,
-                else => error.UnexpectedTokenKind,
-            },
-            Operand.Value.PCOffset11 => switch (value) {
-                .integer => |integer| .{ .resolved = try integer.castTo(i11) },
-                .label => .unresolved,
-                else => error.UnexpectedTokenKind,
-            },
-            Operand.Value.TrapVect => switch (value) {
-                .integer => |integer| .{ .inner = try integer.castTo(u8) },
-                else => error.UnexpectedTokenKind,
-            },
-            else => comptime unreachable,
-        },
-    };
-}
