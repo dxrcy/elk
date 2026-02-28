d9c0158d8444f7f48541e764c3bb3dba745d58fc
darcy <acednes@gmail.com>
2026-02-28 19:05:42 +1100

    chore: root imports files explicitly to test

--- src/root.zig
+++ src/root.zig
@@ -5,5 +5,21 @@ pub const Parser = @import("compile/parse/Parser.zig");
 pub const Runtime = @import("emulate/Runtime.zig");
 
 comptime {
-    @import("std").testing.refAllDecls(@This());
+    _ = &@import("Policies.zig");
+    _ = &@import("compile/statement.zig");
+    _ = &@import("compile/Span.zig");
+    _ = &@import("compile/Air.zig");
+    _ = &@import("compile/parse/TokenIter.zig");
+    _ = &@import("compile/parse/Lexer.zig");
+    _ = &@import("compile/parse/Parser.zig");
+    _ = &@import("compile/parse/integers.zig");
+    _ = &@import("compile/parse/Token.zig");
+    _ = &@import("report/Reporter.zig");
+    _ = &@import("report/Ctx.zig");
+    _ = &@import("report/diagnostic.zig");
+    _ = &@import("emulate/traps.zig");
+    _ = &@import("emulate/Tty.zig");
+    _ = &@import("emulate/NewlineTracker.zig");
+    _ = &@import("emulate/Mask.zig");
+    _ = &@import("emulate/Runtime.zig");
 }
