7a7f2f4978a9cca47dc32db8b16dfd49dba94340
darcy <acednes@gmail.com>
2026-03-04 21:13:33 +1100

    refactor(bin): extract `assemble` and `emulate` functions

--- bin/main.zig
+++ bin/main.zig
@@ -1,5 +1,6 @@
 const std = @import("std");
 const Io = std.Io;
+const Allocator = std.mem.Allocator;
 
 const lcz = @import("lcz");
 
@@ -37,29 +38,7 @@ pub fn main(init: std.process.Init) !u8 {
 
     switch (cli.command) {
         .assemble => {
-            const source = try Io.Dir.cwd().readFileAlloc(io, cli.filepath, gpa, .unlimited);
-            defer gpa.free(source);
-
-            reporter.setSource(source);
-
-            var air: lcz.Air = .init();
-            defer air.deinit(gpa);
-
-            var parser: lcz.Parser = .new(&air, &traps, source, &reporter);
-
-            try parser.parse(gpa);
-            if (reporter.getLevel() == .err) {
-                reporter.showSummary();
-                return 1;
-            }
-
-            parser.resolveLabels();
-            if (reporter.getLevel() == .err) {
-                reporter.showSummary();
-                return 1;
-            }
-
-            reporter.showSummary();
+            const air = try assemble(cli.filepath, &traps, &reporter, io, gpa);
 
             // TODO: Derive from `cli.filepath`
             const obj_path = "hw.obj";
@@ -75,96 +54,99 @@ pub fn main(init: std.process.Init) !u8 {
         },
 
         .emulate => {
-            const obj_file = try Io.Dir.cwd().openFile(io, cli.filepath, .{});
-            var read_buffer: [1024]u8 = undefined;
-
-            var write_buffer: [64]u8 = undefined;
-            var writer = Io.File.stdout().writer(io, &write_buffer);
-            var reader = Io.File.stdin().reader(io, &.{});
-
-            var runtime = try lcz.Runtime.init(
-                &traps,
-                hooks,
-                &policies,
-                &writer.interface,
-                &reader.interface,
-                io,
-                gpa,
-            );
-            defer runtime.deinit(gpa);
-
-            try runtime.readFromFile(obj_file, &read_buffer, io);
-
-            runtime.run() catch |err| switch (err) {
-                error.WriteFailed,
-                error.ReadFailed,
-                error.TermiosFailed,
-                => |err2| return err2,
-                else => |err2| {
-                    std.log.err("runtime threw exception: {t}", .{err2});
-                },
-            };
-
-            try runtime.writer.ensureNewline();
-            try runtime.writer.interface.flush();
+            const file = try Io.Dir.cwd().openFile(io, cli.filepath, .{});
+            try emulate(.{ .file = file }, &traps, hooks, &policies, io, gpa);
         },
 
         .assemble_emulate => {
-            const source = try Io.Dir.cwd().readFileAlloc(io, cli.filepath, gpa, .unlimited);
-            defer gpa.free(source);
-
-            reporter.setSource(source);
-
-            var air: lcz.Air = .init();
-            defer air.deinit(gpa);
-
-            var parser: lcz.Parser = .new(&air, &traps, source, &reporter);
-
-            try parser.parse(gpa);
-            if (reporter.getLevel() == .err) {
-                reporter.showSummary();
-                return 1;
-            }
-
-            parser.resolveLabels();
-            if (reporter.getLevel() == .err) {
-                reporter.showSummary();
-                return 1;
-            }
-
-            reporter.showSummary();
-
-            var write_buffer: [64]u8 = undefined;
-            var writer = Io.File.stdout().writer(io, &write_buffer);
-            var reader = Io.File.stdin().reader(io, &.{});
-
-            var runtime = try lcz.Runtime.init(
-                &traps,
-                hooks,
-                &policies,
-                &writer.interface,
-                &reader.interface,
-                io,
-                gpa,
-            );
-            defer runtime.deinit(gpa);
-
-            try air.emitRuntime(&runtime);
-
-            runtime.run() catch |err| switch (err) {
-                error.WriteFailed,
-                error.ReadFailed,
-                error.TermiosFailed,
-                => |err2| return err2,
-                else => |err2| {
-                    std.log.err("runtime threw exception: {t}", .{err2});
-                },
-            };
-
-            try runtime.writer.ensureNewline();
-            try runtime.writer.interface.flush();
+            const air = try assemble(cli.filepath, &traps, &reporter, io, gpa);
+            try emulate(.{ .air = &air }, &traps, hooks, &policies, io, gpa);
         },
     }
 
     return 0;
+}
+
+fn assemble(
+    filepath: []const u8,
+    traps: *const lcz.Traps,
+    reporter: *lcz.Reporter,
+    io: Io,
+    gpa: Allocator,
+) !lcz.Air {
+    const source = try Io.Dir.cwd().readFileAlloc(io, filepath, gpa, .unlimited);
+    defer gpa.free(source);
+
+    reporter.setSource(source);
+
+    var air: lcz.Air = .init();
+    defer air.deinit(gpa);
+
+    var parser: lcz.Parser = .new(&air, traps, source, reporter);
+
+    try parser.parse(gpa);
+    if (reporter.getLevel() == .err) {
+        reporter.showSummary();
+        return error.ProgramError;
+    }
+
+    parser.resolveLabels();
+    if (reporter.getLevel() == .err) {
+        reporter.showSummary();
+        return error.ProgramError;
+    }
+
+    reporter.showSummary();
+
+    return air;
+}
+
+fn emulate(
+    runtime_source: union(enum) {
+        file: Io.File,
+        air: *const lcz.Air,
+    },
+    traps: *const lcz.Traps,
+    hooks: lcz.Runtime.Hooks,
+    policies: *const lcz.Policies,
+    io: Io,
+    gpa: Allocator,
+) !void {
+    var write_buffer: [64]u8 = undefined;
+    var writer = Io.File.stdout().writer(io, &write_buffer);
+    var reader = Io.File.stdin().reader(io, &.{});
+
+    var runtime = try lcz.Runtime.init(
+        traps,
+        hooks,
+        policies,
+        &writer.interface,
+        &reader.interface,
+        io,
+        gpa,
+    );
+    defer runtime.deinit(gpa);
+
+    switch (runtime_source) {
+        .file => |file| {
+            var read_buffer: [1024]u8 = undefined;
+            try runtime.readFromFile(file, &read_buffer, io);
+        },
+        .air => |air| {
+            try air.emitRuntime(&runtime);
+        },
+    }
+
+    runtime.run() catch |err| switch (err) {
+        error.WriteFailed,
+        error.ReadFailed,
+        error.TermiosFailed,
+        => |err2| return err2,
+        else => |err2| {
+            std.log.err("runtime threw exception: {t}", .{err2});
+        },
+    };
+
+    try runtime.writer.ensureNewline();
+    try runtime.writer.interface.flush();
 }
