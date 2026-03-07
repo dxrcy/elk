cefd30432b360ff666e70f25f1466d3263a7ddc3
darcy <acednes@gmail.com>
2026-03-07 14:12:25 +1100

    feat(runtime/debugger): read command from terminal

--- src/emulate/Runtime.zig
+++ src/emulate/Runtime.zig
@@ -150,9 +150,36 @@ fn invokeDebugger(runtime: *Runtime) !?Control {
 
     try runtime.tty.enableRawMode();
 
-    _ = try runtime.readByte();
+    var buffer: [64]u8 = undefined;
+    var length: usize = 0;
+
+    while (true) {
+        std.debug.print("\r\x1b[K", .{});
+        std.debug.print("{s}", .{buffer[0..length]});
+
+        const char = try runtime.readByte();
+
+        switch (char) {
+            '\n' => break,
+
+            std.ascii.control_code.bs,
+            std.ascii.control_code.del,
+            => if (length > 0) {
+                length -= 1;
+            },
+
+            else => if (length < buffer.len) {
+                buffer[length] = char;
+                length += 1;
+            },
+        }
+    }
 
     try runtime.tty.disableRawMode();
+
+    std.debug.print("\n", .{});
+
+    std.debug.print("[{s}]\n", .{buffer[0..length]});
 
     return null;
 }
