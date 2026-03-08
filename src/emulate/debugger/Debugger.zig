ddd33ad46ba2291e2242c48fb38da7671242266c
darcy <acednes@gmail.com>
2026-03-08 16:46:26 +1100

    refactor(debugger/input): move input reading to new `Input` type

--- src/emulate/debugger/Debugger.zig
+++ src/emulate/debugger/Debugger.zig
@@ -30,69 +30,106 @@ pub fn invoke(debugger: *Debugger, runtime: *Runtime) !?Runtime.Control {
     }
 }
 
+const Input = struct {
+    const Io = std.Io;
+    const assert = std.debug.assert;
+
+    buffer: []u8,
+    length: usize,
+    cursor: usize,
+
+    reader: *Io.Reader,
+
+    fn new(buffer: []u8, reader: *Io.Reader) Input {
+        return .{
+            .buffer = buffer,
+            .length = 0,
+            .cursor = 0,
+            .reader = reader,
+        };
+    }
+
+    fn readByte(input: *Input) !?u8 {
+        var char: u8 = undefined;
+        input.reader.readSliceAll(@ptrCast(&char)) catch |err| switch (err) {
+            error.EndOfStream => return null,
+            else => return error.ReadFailed,
+        };
+        return char;
+    }
+
+    fn readCommand(input: *Input) ![]const u8 {
+        const prompt = "> ";
+
+        while (true) {
+            assert(input.cursor <= input.length);
+
+            std.debug.print("\r\x1b[K", .{});
+            std.debug.print(prompt, .{});
+            std.debug.print("{s}", .{input.buffer[0..input.length]});
+            std.debug.print("\x1b[{}G", .{input.cursor + prompt.len + 1});
+
+            const char = try input.readByte() orelse
+                break;
+
+            switch (char) {
+                '\n' => break,
+
+                control_code.bs,
+                control_code.del,
+                => if (input.cursor > 0) {
+                    // Shift characters down
+                    if (input.cursor < input.length) {
+                        for (input.cursor..input.length) |i| {
+                            input.buffer[i - 1] = input.buffer[i];
+                        }
+                    }
+                    input.cursor -= 1;
+                    input.length -= 1;
+                },
+
+                control_code.esc => {
+                    if (try input.readByte() == '[') {
+                        const command = try input.readByte() orelse
+                            break;
+                        switch (command) {
+                            'C' => if (input.cursor < input.length) {
+                                input.cursor += 1;
+                            },
+                            'D' => if (input.cursor > 0) {
+                                input.cursor -= 1;
+                            },
+                            else => {},
+                        }
+                    }
+                },
+
+                0x20...0x7e => if (input.length < input.buffer.len) {
+                    input.buffer[input.length] = char;
+                    input.length += 1;
+                    input.cursor += 1;
+                },
+
+                else => {},
+            }
+        }
+
+        std.debug.print("\n", .{});
+
+        return input.buffer[0..input.length];
+    }
+};
+
 fn readCommand(debugger: *Debugger, runtime: *Runtime, buffer: []u8) ![]const u8 {
     _ = debugger;
 
-    const prompt = "> ";
-
-    var length: usize = 0;
-    var cursor: usize = 0;
-
     try runtime.tty.enableRawMode();
 
-    while (true) {
-        std.debug.assert(cursor <= length);
+    var input: Input = .new(buffer, runtime.reader);
 
-        std.debug.print("\r\x1b[K", .{});
-        std.debug.print(prompt, .{});
-        std.debug.print("{s}", .{buffer[0..length]});
-        std.debug.print("\x1b[{}G", .{cursor + prompt.len + 1});
+    const line = try input.readCommand();
 
-        const char = try runtime.readByte();
-
-        switch (char) {
-            '\n' => break,
-
-            control_code.bs,
-            control_code.del,
-            => if (cursor > 0) {
-                // Shift characters down
-                if (cursor < length) {
-                    for (cursor..length) |i| {
-                        buffer[i - 1] = buffer[i];
-                    }
-                }
-                cursor -= 1;
-                length -= 1;
-            },
-
-            control_code.esc => {
-                if (try runtime.readByte() == '[') {
-                    const command = try runtime.readByte();
-                    switch (command) {
-                        'C' => if (cursor < length) {
-                            cursor += 1;
-                        },
-                        'D' => if (cursor > 0) {
-                            cursor -= 1;
-                        },
-                        else => {},
-                    }
-                }
-            },
-
-            0x20...0x7e => if (length < buffer.len) {
-                buffer[length] = char;
-                length += 1;
-                cursor += 1;
-            },
-
-            else => {},
-        }
-    }
-
-    std.debug.print("\n", .{});
     try runtime.tty.disableRawMode();
 
-    return buffer[0..length];
+    return line;
 }
