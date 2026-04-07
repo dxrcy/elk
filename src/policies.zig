e671700a3db6f5660d7048566db16c28202f21a5
darcy <acednes@gmail.com>
2026-04-07 12:36:50 +1000

    feat(policies): add methods for parsing and access

--- src/policies.zig
+++ src/policies.zig
@@ -1,3 +1,6 @@
+const std = @import("std");
+const assert = std.debug.assert;
+
 pub const Policies = packed struct {
     pub const Policy = enum(u1) { permit, forbid };
 
@@ -76,5 +79,80 @@ pub const Policies = packed struct {
         for (@typeInfo(T).@"struct".fields) |field|
             @field(filled, field.name) = value;
         return filled;
+    }
+
+    pub fn get(policies: *const Policies, category: []const u8, item: []const u8) ?Policy {
+        inline for (@typeInfo(Policies).@"struct".fields) |category_field| {
+            if (std.mem.eql(u8, category_field.name, category)) {
+                inline for (@typeInfo(category_field.type).@"struct".fields) |item_field| {
+                    if (std.mem.eql(u8, item_field.name, item)) {
+                        return @field(@field(policies, category_field.name), item_field.name);
+                    }
+                }
+            }
+        }
+        return null;
+    }
+
+    pub fn set(policies: *Policies, category: []const u8, item: []const u8, policy: Policy) bool {
+        inline for (@typeInfo(Policies).@"struct".fields) |category_field| {
+            if (std.mem.eql(u8, category_field.name, category)) {
+                inline for (@typeInfo(category_field.type).@"struct".fields) |item_field| {
+                    if (std.mem.eql(u8, item_field.name, item)) {
+                        @field(@field(policies, category_field.name), item_field.name) = policy;
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    pub fn unionWith(lhs: Policies, rhs: Policies) Policies {
+        var result: Policies = .none;
+        inline for (@typeInfo(Policies).@"struct".fields) |category| {
+            inline for (@typeInfo(category.type).@"struct".fields) |item| {
+                const lhs_item = lhs.get(category.name, item.name) orelse unreachable;
+                const rhs_item = rhs.get(category.name, item.name) orelse unreachable;
+                if (lhs_item == .permit or rhs_item == .permit)
+                    assert(result.set(category.name, item.name, .permit) == true);
+            }
+        }
+        return result;
+    }
+
+    pub fn parseList(string: []const u8) !Policies {
+        var policies: Policies = .none;
+
+        var words = std.mem.tokenizeScalar(u8, string, ',');
+        while (words.next()) |word| {
+            if (std.mem.cutPrefix(u8, word, "+")) |predef_name| {
+                const predef = resolvePredef(predef_name) orelse
+                    return error.InvalidPredefName;
+                policies = policies.unionWith(predef);
+                continue;
+            }
+
+            var segmentts = std.mem.tokenizeScalar(u8, word, '.');
+            const category = segmentts.next() orelse
+                return error.MalformedPolicyName;
+            const item = segmentts.next() orelse
+                return error.MalformedPolicyName;
+            if (segmentts.next() != null)
+                return error.MalformedPolicyName;
+
+            if (!policies.set(category, item, .permit))
+                return error.InvalidPolicyName;
+        }
+
+        return policies;
+    }
+
+    fn resolvePredef(name: []const u8) ?Policies {
+        inline for (@typeInfo(predefs).@"struct".decls) |predef| {
+            if (std.mem.eql(u8, predef.name, name))
+                return @field(predefs, predef.name);
+        }
+        return null;
     }
 };
