8fa2e9e8c3ca00dfb60aa05ef1e136f96ab6ef4a
darcy <acednes@gmail.com>
2026-07-03 15:24:51 +1000

    refactor(provider): extract `resolveFieldLabel` submethods

--- lib/provider.zig
+++ lib/provider.zig
@@ -3,6 +3,8 @@ const assert = std.debug.assert;
 
 const Reporter = @import("reporting/reporting.zig").Primary;
 const Air = @import("compile/Air.zig");
+const Span = @import("compile/Span.zig");
+const Operand = @import("compile/Operand.zig");
 const Source = @import("compile/Source.zig");
 
 pub const Provider = union(enum) {
@@ -78,73 +80,100 @@ pub const Provider = union(enum) {
             .resolved => return,
         }
 
-        const string = operand.span.view(source);
-
-        const offset = blk: switch (provider) {
+        const offset = switch (provider) {
             .none => {
                 // TODO: Report properly
                 std.log.err("label operand cannot be resolved", .{});
                 return error.Reported;
             },
 
-            .assembly => |assembly| {
-                const definition =
-                    assembly.air.findLabel(.exact, string, assembly.source) orelse {
-                        const nearest: Reporter.Diagnostic.NearestSpan =
-                            if (assembly.air.findLabel(.nearest, string, assembly.source)) |label|
-                                if (std.ascii.eqlIgnoreCase(string, label.span.view(assembly.source)))
-                                    .{ .case_insensitive = label.span }
-                                else
-                                    .{ .edit_distance = label.span }
-                            else
-                                .none;
-                        try reporter.report(.undefined_label, .{
-                            .reference = operand.span,
-                            .nearest = nearest,
-                            .definition_source = assembly.source,
-                        }).abort();
-                    };
-                const definition_address = definition.index + assembly.air.origin;
-
-                const offset = calculateOffset(Int, definition_address, address) orelse {
-                    try reporter.report(.offset_too_large, .{
-                        .reference = operand.span,
-                        .definition = definition.span,
-                        .offset = calculateOffset(i17, definition_address, address) orelse
-                            unreachable,
-                        .bits = @typeInfo(Int).int.bits,
-                        .definition_source = assembly.source,
-                    }).abort();
-                };
-
-                if (increment_references)
-                    definition.references += 1;
-
-                break :blk offset;
-            },
-
-            .symbols => |symbols| {
-                // TODO: Provide suggestion for nearest match
-                const definition = (getSymbolAddress(string, symbols) orelse {
-                    try reporter.report(.undefined_label, .{
-                        .reference = operand.span,
-                        .nearest = .none,
-                        .definition_source = .empty,
-                    }).abort();
-                });
-
-                const offset = calculateOffset(Int, definition, address) orelse {
-                    // TODO: Report properly
-                    std.log.err("offset too large", .{});
-                    return error.Reported;
-                };
-                break :blk offset;
-            },
+            .assembly => |assembly| try resolveFieldAssembly(
+                Int,
+                operand.span,
+                address,
+                assembly,
+                source,
+                reporter,
+                increment_references,
+            ),
+            .symbols => |symbols| try resolveFieldSymbols(Int, operand.span, address, symbols, source, reporter),
         };
 
         operand.value = .{ .resolved = .{ .integer = offset, .form = null } };
     }
 };
+
+fn resolveFieldAssembly(
+    comptime Int: type,
+    operand: Span,
+    address: usize,
+    assembly: Provider.Assembly,
+    source: Source,
+    reporter: *Reporter,
+    comptime increment_references: bool,
+) error{Reported}!Int {
+    const string = operand.view(source);
+
+    const definition =
+        assembly.air.findLabel(.exact, string, assembly.source) orelse {
+            const nearest: Reporter.Diagnostic.NearestSpan =
+                if (assembly.air.findLabel(.nearest, string, assembly.source)) |label|
+                    if (std.ascii.eqlIgnoreCase(string, label.span.view(assembly.source)))
+                        .{ .case_insensitive = label.span }
+                    else
+                        .{ .edit_distance = label.span }
+                else
+                    .none;
+            try reporter.report(.undefined_label, .{
+                .reference = operand,
+                .nearest = nearest,
+                .definition_source = assembly.source,
+            }).abort();
+        };
+
+    const definition_address = definition.index + assembly.air.origin;
+    const offset = calculateOffset(Int, definition_address, address) orelse {
+        try reporter.report(.offset_too_large, .{
+            .reference = operand,
+            .definition = definition.span,
+            .offset = calculateOffset(i17, definition_address, address) orelse
+                unreachable,
+            .bits = @typeInfo(Int).int.bits,
+            .definition_source = assembly.source,
+        }).abort();
+    };
+
+    if (increment_references)
+        definition.references += 1;
+
+    return offset;
+}
+
+fn resolveFieldSymbols(
+    comptime Int: type,
+    operand: Span,
+    address: usize,
+    symbols: []const Provider.SymbolEntry,
+    source: Source,
+    reporter: *Reporter,
+) error{Reported}!Int {
+    const string = operand.view(source);
+
+    // TODO: Provide suggestion for nearest match
+    const definition = (Provider.getSymbolAddress(string, symbols) orelse {
+        try reporter.report(.undefined_label, .{
+            .reference = operand,
+            .nearest = .none,
+            .definition_source = .empty,
+        }).abort();
+    });
+
+    return calculateOffset(Int, definition, address) orelse {
+        // TODO: Report properly
+        std.log.err("offset too large", .{});
+        return error.Reported;
+    };
+}
 
 fn calculateOffset(comptime T: type, definition: usize, reference: usize) ?T {
     comptime assert(@typeInfo(T).int.signedness == .signed);
