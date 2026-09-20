2a2cc80b136f7a783e31eae80db3d6bfe79eb397
Tom Lynch <twhlynch@gmail.com>
2026-09-20 10:57:55 +1000

    chore: rename `--random-memory` to `--random-init`

--- cli/Cli.zig
+++ cli/Cli.zig
@@ -158,8 +158,8 @@ const template = .{
         .short = 'd',
         .long = "debug",
     },
-    .random_memory = zilc.Flag{
-        .long = "random-memory",
+    .random_init = zilc.Flag{
+        .long = "random-init",
         .value = zilc.types.integer(u64),
     },
     .patch_symbols = zilc.Flag{
@@ -323,7 +323,7 @@ pub fn parse(
     return .{
         .operation = operation,
         .policies = if (options.flags.permit) |policies| policies else .none,
-        .random_seed = options.flags.random_memory,
+        .random_seed = options.flags.random_init,
         .strictness = if (options.flags.strict)
             .strict
         else if (options.flags.relaxed)
@@ -349,7 +349,7 @@ fn checkDependencies(options: *const zilc.Options(template)) !void {
     try zilc.checkDependencies(.export_listing, enum { assemble }, enum {}, &options.flags);
     try zilc.checkDependencies(.trap_aliases, enum { assemble, check, format }, enum {}, &options.flags);
     try zilc.checkDependencies(.debug, enum {}, enum { assemble, check, clean, format, lsp }, &options.flags);
-    try zilc.checkDependencies(.random_memory, enum {}, enum { assemble, check, clean, format, lsp }, &options.flags);
+    try zilc.checkDependencies(.random_init, enum {}, enum { assemble, check, clean, format, lsp }, &options.flags);
     try zilc.checkDependencies(.input_partial, enum { debug }, enum { input_full }, &options.flags);
     try zilc.checkDependencies(.input_full, enum { debug }, enum { input_partial }, &options.flags);
     try zilc.checkDependencies(.history_file, enum { debug }, enum {}, &options.flags);
